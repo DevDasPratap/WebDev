559572c90b75e1e27a75226f585f14f30a4ad765
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-07-15 20:42:22 +0530

    Create a.js

---
+++ JavaScript/a.js
@@ -0,0 +1,63 @@
+// function reverseWords(str) {
+//   let reverseWordArr = str.split(" ").map(word => word.split("").reverse().join(""));
+//   return reverseWordArr.join(" ");
+// }
+// let a=reverseWords('i am pratap das');
+// console.log(a);
+
+// function wordsReverser(string){
+//     return string.split('').reverse().join('');
+//   }
+//   console.log(wordsReverser('I am pratap das'));
+
+
+// function wordsReverser(string){
+// return string.split("").reverse().join("").split(" ").reverse().join(" ")  
+// }
+
+// console.log(wordsReverser('New string, same results.'));
+
+// function wordsReverser(string){
+//     var arr = string.split("");
+//     var output = [];
+//     for(var i = arr.length - 1;  i >= 0; i--){
+//         output.push(arr[i]);
+//     }
+//   return output.join("");
+// }
+// let a=wordsReverser('I am pratap das');
+// console.log(a);
+
+
+// function reverseWord (sentence) {
+//   return sentence.split(' ').map(function(word) {
+//     return word.split('').reverse().join('');
+//   }).join(' ');
+// }
+
+// console.log(reverseWord("I am pratap das"));
+
+
+// function reverse(s){
+//     return s.split("").reverse().join("");
+// }
+// console.log(reverse('i am pratap das'));
+
+
+
+// function reverse(s){
+//     return [...s].reverse().join("");
+// }
+// console.log(reverse('i am das'))
+
+
+
+
+// var s= ["i", "like", "this", "program", "very", "much"];
+let s='i am pratap das';
+    var ans ="";
+    for (var i = 4; i >= 0; i--)
+    {
+        ans += s[i] + " ";
+    }
+    console.log(ans.slice(0,ans.length-1));
